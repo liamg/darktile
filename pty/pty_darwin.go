2ee5aeda8b99b2c8bf374714b2272720852ce64e
Liam Galvin <liam.galvin@switchconcepts.com>
2018-08-13 17:42:02 +0100

    try to fix darwin pty

--- pty/pty_darwin.go
+++ pty/pty_darwin.go
@@ -59,7 +59,7 @@ func ptsname(file *os.File) (name string, err error) {
 	return "", errors.New("TIOCPTYGNAME string not NUL-terminated")
 }
 
-func ioctl(file *os.File, command uint, arg int) (int, error) {
+func ioctl(file *os.File, command uint, arg uintptr) (int, error) {
 	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(file.Fd()),
 		uintptr(command), uintptr(unsafe.Pointer(&arg)))
 	if err != 0 {
