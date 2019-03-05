c975ebbaabd0fee5fa37bd5e7763bf65e7379ebc
Menno Finlay-Smits <inbox@menno.io>
2019-03-06 12:05:41 +1300

    Add option for generating CPU profile

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
 	"github.com/liamg/aminal/gui"
 	"github.com/liamg/aminal/platform"
 	"github.com/liamg/aminal/terminal"
 	"github.com/riywo/loginshell"
-	"os"
-	"runtime"
 )
 
 type callback func(terminal *terminal.Terminal, g *gui.GUI)
@@ -29,8 +32,13 @@ func initialize(unitTestfunc callback) {
 	}
 	defer logger.Sync()
 
-	logger.Infof("Allocating pty...")
+	if conf.CPUProfile != "" {
+		logger.Infof("Starting CPU profiling...")
+		stop := startCPUProf(conf.CPUProfile)
+		defer stop()
+	}
 
+	logger.Infof("Allocating pty...")
 	pty, err := platform.NewPty(80, 25)
 	if err != nil {
 		logger.Fatalf("Failed to allocate pty: %s", err)
@@ -77,4 +85,13 @@ func initialize(unitTestfunc callback) {
 	if err := g.Render(); err != nil {
 		logger.Fatalf("Render error: %s", err)
 	}
+}
+
+func startCPUProf(filename string) func() {
+	profileFile, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(profileFile)
+	return pprof.StopCPUProfile
 }
